0e24ad58e262abb94d80dbef1f2895fcdbfa61e6
Utashi Fujimoto <frusciante2580@gmail.com>
2024-06-02 11:15:00 +0000

    fix: initial creation of pk.json

--- ethnode/ganache.sh
+++ ethnode/ganache.sh
@@ -8,6 +8,15 @@ INSTALL_DIR="$ROOT_DIR/ethnode/install"
 ACCOUNT_KEYS_PATH="$PRIVATE_DIR/ganache_pk.json"
 CONFIG_PATH="$ROOT_DIR/config.json"
 
+# create ganache_pk.json
+if [ ! -f "$ACCOUNT_KEYS_PATH" ]; then
+    echo "ganache_pk.json not found in $ACCOUNT_KEYS_PATH, creating it..."
+    mkdir -p "$PRIVATE_DIR"    
+    touch "$ACCOUNT_KEYS_PATH"
+    echo "{}" > "$ACCOUNT_KEYS_PATH"
+    echo "Empty ganache_pk.json file created in $ACCOUNT_KEYS_PATH"
+fi
+
 # load config values from config.json
 GANACHE_CHAIN_ID=$(jq -r '.NETWORK.GANACHE.chain_id' "$CONFIG_PATH")
 GANACHE_PORT=$(jq -r '.NETWORK.GANACHE.rpc_port' "$ROOT_DIR/config.json")
