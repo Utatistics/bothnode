17d59b7d72f4d0ebcd4e7e822fd07e05ea77e1af
Utashi Fujimoto <frusciante2580@gmail.com>
2024-08-27 16:20:32 +0000

    fix: missing chain-id

--- ethnode/clef.sh
+++ ethnode/clef.sh
@@ -4,8 +4,13 @@
 ROOT_DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
 PRIVATE_DIR="$ROOT_DIR/private"
 
+# set parameters
+NETWORK=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+CHAIN_ID=$(jq -r --arg NETWORK "$NETWORK" '.NETWORK[$NETWORK].chain_id' "$PATH_TO_CONFIG")
+
 # generate an account key pair while specifying where to store them
 clef newaccount --keystore $PRIVATE_DIR/keystore
 
 echo '>>> Starting clef...'
 nohup clef --keystore $PRIVATE_DIR/keystore --configdir $PRIVATE_DIR/clef --chainid $CHAIN_ID > $HOME/.bothnode/log/clef.log 2>&1 &
+ok
