3b2aee66509e45ec4685a09aeac05aeaddb69c5a
Utashi Fujimoto <frusciante2580@gmail.com>
2024-08-27 20:07:20 +0000

    debug: private dir not found.

--- ethnode/clef.sh
+++ ethnode/clef.sh
@@ -5,10 +5,14 @@ ROOT_DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
 PRIVATE_DIR="$ROOT_DIR/private"
 PATH_TO_CONFIG="$ROOT_DIR/config.json"
 
+echo $ROOT_DIR
+echo $PATH_TO_CONFIG
 
 # set parameters
 NETWORK=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 CHAIN_ID=$(jq -r --arg NETWORK "$NETWORK" '.NETWORK[$NETWORK].chain_id' "$PATH_TO_CONFIG")
+
+mkdir -p $PRIVATE_DIR/keystore
 
 # generate an account key pair while specifying where to store them
 clef newaccount --keystore $PRIVATE_DIR/keystore
