6892f50f466fd3e42227cf4f0a841215842ba0fc
Utashi Fujimoto <frusciante2580@gmail.com>
2024-10-12 01:20:03 +0000

    rename: contract folder

---
+++ backend/solc/Sample/Main.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract BalanceChecker {
+    function getBalance(address account) public view returns (uint256) {
+        return account.balance;
+    }
+}
+
