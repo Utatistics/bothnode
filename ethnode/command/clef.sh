5a670a4d4607cc5852b6566f84f1058fdecd696c
Utashi Fujimoto <frusciante2580@gmail.com>
2024-11-03 22:16:59 +0000

    fix: path modifications

--- ethnode/command/clef.sh
+++ ethnode/command/clef.sh
@@ -10,10 +10,10 @@ NETWORK=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 CHAIN_ID=$(jq -r --arg NETWORK "$NETWORK" '.NETWORK[$NETWORK].chain_id' "$PATH_TO_CONFIG")
 
 # create the clef directory
-mkdir -p $CLEF_DIR
+#mkdir -p $CLEF_DIR
 
 # generate an account key pair while specifying where to store them
-clef newaccount --keystore $CLEF_DIR/keystore
+#clef newaccount --keystore $CLEF_DIR/keystore
 
 # start clef
-clef --keystore $CLEF_DIR/keystore --configdir $CLEF_DIR/clef --chainid $CHAIN_ID 
+#clef --keystore $CLEF_DIR/keystore --configdir $CLEF_DIR/clef --chainid $CHAIN_ID 
